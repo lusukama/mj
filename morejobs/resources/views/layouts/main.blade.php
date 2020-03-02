31c9a3e0cc6cdec11a097848adde99d87c44ef24
selemani1 <lusukamaselemani@gmail.com>
2020-03-02 14:55:14 +0200

    Add Navbar

--- morejobs/resources/views/layouts/main.blade.php
+++ morejobs/resources/views/layouts/main.blade.php
@@ -6,17 +6,17 @@
   <meta http-equiv="x-ua-compatible" content="ie=edge">
   <title>More Jobs | Home </title>
   <!-- MDB icon -->
-  <link rel="icon" href="img/mdb-favicon.ico" type="image/x-icon">
+  <link rel="icon" href="public/doc/img/mdb-favicon.ico" type="image/x-icon">
   <!-- Font Awesome -->
   <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.11.2/css/all.css">
   <!-- Google Fonts Roboto -->
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap">
   <!-- Bootstrap core CSS -->
-  <link rel="stylesheet" href="css/bootstrap.min.css">
+  <link rel="stylesheet" href="{{ asset('public/doc/css/bootstrap.min.css')}}">
   <!-- Material Design Bootstrap -->
-  <link rel="stylesheet" href="css/mdb.min.css">
+  <link rel="stylesheet" href="{{ asset('public/doc/css/mdb.min.css')}}">
   <!-- Your custom styles (optional) -->
-  <link rel="stylesheet" href="css/style.css">
+  <link rel="stylesheet" href="{{ asset('public/doc/css/style.css')}}">
 </head>
 <body>
 
@@ -26,18 +26,18 @@
   <!-- dynamic content will go here  -->
   @yield('content')
 
-  
+
   @include('layouts.footer')
   <!-- End your project here-->
 
   <!-- jQuery -->
-  <script type="text/javascript" src="js/jquery.min.js"></script>
+  <script type="text/javascript" src="{{ asset('public/doc/js/jquery.min.js')}}"></script>
   <!-- Bootstrap tooltips -->
-  <script type="text/javascript" src="js/popper.min.js"></script>
+  <script type="text/javascript" src="{{ asset('public/doc/js/popper.min.js')}}"></script>
   <!-- Bootstrap core JavaScript -->
-  <script type="text/javascript" src="js/bootstrap.min.js"></script>
+  <script type="text/javascript" src="{{ asset('public/doc/js/bootstrap.min.js')}}"></script>
   <!-- MDB core JavaScript -->
-  <script type="text/javascript" src="js/mdb.min.js"></script>
+  <script type="text/javascript" src="{{ asset('public/doc/js/mdb.min.js')}}"></script>
   <!-- Your custom scripts (optional) -->
   <script type="text/javascript"></script>
 
--- morejobs/resources/views/layouts/navBar.blade.php
+++ morejobs/resources/views/layouts/navBar.blade.php
@@ -1,1 +1,49 @@
-<!-- navbar here -->
+<!--Navbar-->
+<nav class="navbar fixed-top navbar-expand-lg navbar-dark primary-color">
+  <!-- Navbar brand -->
+  <a class="navbar-brand" href="#">more<span class="job">Jobs</span></a>
+  <!-- Collapse button -->
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#basicExampleNav"
+    aria-controls="basicExampleNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <!-- Collapsible content -->
+  <div class="collapse navbar-collapse" id="basicExampleNav">
+
+    <!-- Links -->
+    <ul class="navbar-nav ml-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Home
+          <span class="sr-only">(current)</span>
+        </a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Add Jobs</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Apply For Jobs</a>
+      </li>
+
+      <!-- Dropdown -->
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" id="navbarDropdownMenuLink" data-toggle="dropdown"
+          aria-haspopup="true" aria-expanded="false">Category</a>
+        <div class="dropdown-menu dropdown-primary" aria-labelledby="navbarDropdownMenuLink">
+          <a class="dropdown-item" href="#">IT Jobs</a>
+          <a class="dropdown-item" href="#">Builders Jobs</a>
+          <a class="dropdown-item" href="#">Publi Jobs</a>
+        </div>
+      </li>
+
+    </ul>
+    <!-- Links -->
+    <form class="form-inline">
+      <div class="md-form my-0">
+        <input class="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search">
+      </div>
+    </form>
+  </div>
+  <!-- Collapsible content -->
+</nav>
+<!--/.Navbar-->
